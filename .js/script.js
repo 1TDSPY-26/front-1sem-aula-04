637a4b34afe979fe70c8d952d1da1fe5fa63f880
GustavoRapozo <gustavorapozo2004@gmail.com>
2026-05-13 21:00:38 -0300

    feat : Ajustando a area de login com javascript

--- .js/script.js
+++ .js/script.js
@@ -1,10 +1,115 @@
-const pessoa = {
-    nome : "Gustavo",
-    idade : 21,
-    casado : false,
-    email : "gustavo@example.com"
+
+// // Conversões:
+// let numero = "123";
+// console.log(typeof numero); // 123
+// console.log(typeof parseInt(numero)); // 123
+// console.log(typeof parseFloat("12.5")); // 12.5
+// console.log(typeof numero.toString()); // “123”
+
+// console.log("TIPOS"); 
+// console.log(typeof null); 
+// console.log(typeof {});  
+// console.log(typeof []); 
+// console.log(typeof 'a');
+// console.log(typeof true);
+
+// // reatribuição de valores direto:
+// numero = false;
+// console.log(numero);
+
+// // Métodos de texto:
+// let frase = "Estão chegando as provas!";
+// console.log(frase.length); // 26 – Retorna a quantidade de caracteres da string
+// console.log(frase.indexOf("as")); // 15 – Retorna a posição da string encontrada
+// console.log(frase.replace("provas", "férias")); // “Estão chegando as férias!”
+// // Formatação de número:
+// let valor = 123.456;
+// console.log(valor.toFixed(2)); // “123.46”
+// console.log(valor.toPrecision(4)); // “123.5”
+
+//Diferença entre var e let
+//HOISTING
+//Não utilizar var por causa do HOISTING
+// var nome1 = "Alê";
+// let nome2 = "Jonas";
+
+// if(true){
+//     var nome1 = "Carlos";
+//     let nome2 = "Matos";
+// }
+
+// console.log(nome1);
+// console.log(nome2);
+
+// const pessoa = {
+//     nome  : "José",
+//     idade : 33,
+//     casado: true,
+//     email : "email@email.com"
+// }
+
+// //Imprimindo os dados do objeto:
+// //Concatenação padrão:
+// console.log(pessoa.nome + " tem " + pessoa.idade + " anos de idade e " + (pessoa.casado ? "é casado " : "não é casado " ) + " caso queira entrar em contato com ele envie um email para " + pessoa.email);
+
+// //Template literals com interpolação:
+// console.log(`${pessoa.nome}  tem  ${pessoa.idade}  anos de idade e  ${(pessoa.casado ? "é casado " : "não é casado " )}, caso queira entrar em contato com ele envie um email para ${pessoa.email}`);
+
+
+// //Alterando um dado do objeto:
+// pessoa.nome = "José da Couves";
+// console.log(pessoa.nome);
+// console.table(pessoa);
+
+//nconst botaoEntrar = document.getElementById("btnEntrar");
+
+// console.log(botaoEntrar.textContent);
+// botaoEntrar.textContent = "OK";
+// console.log(botaoEntrar.textContent);
+
+// botaoEntrar.style.backgroundColor = "yellow";
+
+//   function soma(){
+//      let result = 2+2
+//      console.log(result);
+//       return (2+2);
+//  }
+
+const usuario = {
+    id:1,
+    email: "email@email.com",
+    senha: "12345",
+    nome: "Gustavo Rapozo"
 }
 
-console.log(pessoa.nome + " tem " + pessoa.idade + " anos " + (pessoa.casado ? "e é casado." : "e não é casado.") + " Seu email é: " + pessoa.email);
+const botaoEntrar = document.getElementById("btnEntrar");
 
-console.log(`${pessoa.nome} tem ${pessoa.idade} anos ${(pessoa.casado ? "e é casado." : "e não é casado.")} Seu email é: ${pessoa.email}`);
+botaoEntrar.addEventListener("click", som);function(evento){
+
+    evento.preventDefault();
+
+    const email = document.getElementById("idEmail");
+    const senha = document.getElementById("idSenha");
+
+    console.log(email.value);
+    console.log(senha.value);
+
+    //setTimeout( ()=>{
+    //    document.getElementsByTagName("form")[0]
+    //},5000 );
+
+    try {
+        if (condition) {
+             if( (usuario.email === email.value) && (usuario.senha === senha.value) ){
+                alert("login realizado com sucesso!");
+                window.location.href = "../index.html"
+            }else{
+                throw new error("Senha e email inválidos.");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+    
