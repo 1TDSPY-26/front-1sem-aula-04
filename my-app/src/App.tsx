3b27f169004b4718ffccece8d9c3ddce22fe1a3e
leonardo-neto27 <leonardofilhoneto07@gmail.com>
2026-08-12 21:03:50 -0300

    feat :  Meu primeiro componente REACT

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import Leo from "./components/Leo/Leo";
+
+export default function App() {
+
+
+  return(
+    <div>
+      {
+      }
+      <h1>Meu componente</h1>
+        <Leo/>
+    </div>
+
+  );
+}
---
+++ my-app/src/components/Leo/Leo.tsx
@@ -0,0 +1,6 @@
+
+export default function Leo(){
+    return(
+        <h2>Pastor Xuxuuuuu</h2>
+    );
+}
